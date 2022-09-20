95d1bd7a2ab90d9cdaab10b3625514aaa94df506
jlbrod <109424471+jlbrod@users.noreply.github.com>
2022-09-20 13:01:33 +1000

    UPDATE: USerSchema with all form entries.

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
 const {Schema, model} = require("../db/connection") // import Schema & model
 
-// User Schema
+// User Schema - (Form registration)
 const UserSchema = new Schema({
-    username: {type: String, unique: true, required: true},
+    
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    home_address: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
     password: {type: String, required: true}
+
 })
 
 // User model
