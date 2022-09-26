9d805f1d3e0050b73aac4005e704f33676fa9226
MD KAVIUL HOSSAIN <kaviuln@gmail.com>
2022-09-26 13:53:24 +1000

    Updates

---
+++ README.md
@@ -0,0 +1,24 @@
+# SafeHome_Login
+SafeHome Login Codes
+
+# SIT725 - SafeHome
+
+## Pre-requisite
+Install nodejs with all package including npm
+
+## Run The Appllication
+
+Open the project in visual studio code
+
+```
+$ npm install express --save
+$ npm install cors mongodb dotenv mocha chai request jwt nodemon --save
+$ npm run start
+```
+### Steps to check the website active 
+- paste the url on a browswer : http://localhost:8080 - to demnostrate the database activities  
+
+## Copyrights
+This is a public project that mean anyone can use this project for your working
+
+Copyright (c) 2022 MD KAVIUL HOSSAIN
---
+++ dbConnect.js
@@ -0,0 +1,20 @@
+// require('dotenv').config()
+
+// const MongoClient = require('mongoDb').MongoClient; 
+
+// const url = "mongodb+srv://kaviuln1:kaviuln@kncluster-1.ieurp6n.mongodb.net/?retryWrites=true&w=majority"
+// const client = new MongoClient(url, {useNewUrlParser: true});
+
+// client.connect((err,db) => {
+//    // projectCollection = client.db().collection(collectionName);
+//     if(!err) {
+//         console.log('Database MongoDb Connected!')
+//     }
+//     else{
+//         console.log("No DB connected, err: ", err);
+//         process.exit(1);
+//     }
+// })
+
+// module.MongoClient = client;
+
