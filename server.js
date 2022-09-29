deec02bc7b4ed5100a09dc7ab3cacd324046d265
abyscaria <aby.scaria@gmail.com>
2022-09-29 22:33:39 +1000

    aby-updating
    abys updates

--- server.js
+++ server.js
@@ -18,21 +18,6 @@ app.use(express.static(__dirname + '/public'));
 app.use('/api/projects',projectsRoute)
 
 
-app.get("/test", function (request, response) {
-  var user_name = request.query.user_name;
-  response.end("Hello " + user_name + "!");
-});
-
-app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-  var firstNumber = parseInt(req.params.firstNumber) 
-  var secondNumber = parseInt(req.params.secondNumber)
-  var result = firstNumber + secondNumber || null
-  if(result == null) {
-    res.json({result: result, statusCode: 400}).status(400)
-  }
-  else { res.json({result: result, statusCode: 200}).status(200) } 
-})
-
 //socket test
 io.on('connection', (socket) => {
   console.log('a user connected', socket.id);
