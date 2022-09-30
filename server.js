9b754fdc3d3c0a9fdea3d50f671a906fd0aae384
jlbrod <109424471+jlbrod@users.noreply.github.com>
2022-09-30 11:23:37 +1000

    UPDATE:  Jason + Patch and fix server.js

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-let express = require("express");
-let app = express();
-let dbConnect = require("./dbConnect");
-let http = require('http').createServer(app);
-let io = require('socket.io')(http);
+
+
+require("dotenv").config() // load .env variables
+var express = require("express") // import express
+const morgan = require("morgan") //import morgan
+const {log} = require("mercedlogger") // import mercedlogger's log function
+const cors = require("cors") // import cors
+const UserRouter = require("./controllers/User") //import User Routes
+const TodoRouter = require("./controllers/Todo") // import Todo Routes
+const {createContext} = require("./controllers/middleware")
+
 
 //DESTRUCTURE ENV VARIABLES WITH DEFAULT VALUES
 const {PORT = 3000} = process.env
+
+// Create Application Object
+const app = express()
+
+//ABY server connection and socket IO
+let dbConnect = require("./dbConnect");
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 // GLOBAL MIDDLEWARE
 app.use(express.static(__dirname+'/public'))
@@ -15,22 +29,11 @@ app.use(morgan("tiny")) // log the request for debugging
 app.use(express.json()) // parse json bodies
 app.use(createContext) // create req.context
 
-
-
 app.get("/test", function (request, response) {
   var user_name = request.query.user_name;
   response.end("Hello " + user_name + "!");
 });
 
-app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
-  var firstNumber = parseInt(req.params.firstNumber) 
-  var secondNumber = parseInt(req.params.secondNumber)
-  var result = firstNumber + secondNumber || null
-  if(result == null) {
-    res.json({result: result, statusCode: 400}).status(400)
-  }
-  else { res.json({result: result, statusCode: 200}).status(200) } 
-})
 
 //socket test
 io.on('connection', (socket) => {
@@ -44,8 +47,7 @@ io.on('connection', (socket) => {
 
 });
 
-//Abys Routes
-// routes
+//ABY Routes
 let projectsRoute = require('./routes/projects')
 app.use('/api/projects',projectsRoute)
 
